6ccf303ad382d597466574641c35766de49dd54c
graciebeal <gracie_beal@yahoo.com>
2019-03-24 23:14:07 -0700

    addedv1

---
+++ main.js
@@ -0,0 +1,13 @@
+let restaurantTemplateString = document.getElementById('restaurant-template').innerHTML;
+let renderRestaurant = Handlebars.compile(restaurantTemplateString);
+
+	$.getJSON('https://davidtang.io/teaching/2018/api/restaurants.json').then(
+		function(restaurants){
+			console.log(restaurants);
+			let renderedRestaurant = renderRestaurant({
+				restaurant: restaurants.data
+		});
+
+		$('body').append(renderedRestaurant);
+
+	});
